de54a93f4c70ee3cc65fa991b372c8c17ecb2054
Kristin <kristinlellis9@gmail.com>
2022-08-15 12:21:27 -0400

    updating app js and index html with latest rules for user input

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,13 +1,54 @@
 (function () {
     'use strict';
-    
+
     angular.module('LunchCheck', [])
-    
-    .controller('LunchCheckController', function ($scope) {
-      
-    
-    });
-    
-    
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope', '$filter'];
+    function LunchCheckController($scope, $filter) {
+        $scope.userInput = '';
+
+        /**
+         * @name checkUserEnteredValues
+         * @description split user entered string into array of values separated by comma
+         */
+        $scope.checkUserEnteredValues = function () {
+            if ($scope.userInput === '') {
+                $scope.message = 'Please enter data first';
+            } else {
+                const arrayOfUserEnteredValues = $scope.userInput.split(',');
+                $scope.validateUserInputLength(arrayOfUserEnteredValues);
+            }
+        }
+
+        /**
+         * @name getCountOfNonEmptyValues
+         * @description get count of user entered values that are not empty strings 
+         * in between commas
+         * @return {Number} count of actual values entered 
+         */
+        $scope.getCountOfNonEmptyValues = function (array) {
+            let count = 0;
+            angular.forEach(array, function (value) {
+                if (value.trim().length !== 0) {
+                    count++;
+                }
+            });
+            return count;
+        }
+
+        /**
+         * @name validateUserInputLength
+         * @description check length of array to determine which message to display
+         */
+        $scope.validateUserInputLength = function (array) {
+            let finalCount = $scope.getCountOfNonEmptyValues(array);
+            if (finalCount <= 3 && finalCount !== 0) {
+                $scope.message = 'Enjoy!';
+            } else if (finalCount > 3) {
+                $scope.message = 'Too much!';
+            }
+        }
+
+    }
 })();
-    
